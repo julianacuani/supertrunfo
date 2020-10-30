5f08679c212d70500ef1754d454314768284e298
Juliana Cuani <juliana.271@hotmail.com>
2020-10-29 22:03:16 -0300

    adicionando funções

--- app/src/main/java/com/wcc/supertrunfo/Card.kt
+++ app/src/main/java/com/wcc/supertrunfo/Card.kt
@@ -5,21 +5,47 @@ class Card (
     val driver: Driver,
     val player: Player
 )  {
-    val label: String = "Card ${player.name}"
-    val maxVelocity = setMaxVelocity()
-    val accelerationTime = setAccelerationTime()
-    val passengers = setPassengers()
 
-    private fun setPassengers(): Int {
+    val label: String = "Card ${player.name}"
+    val maxVelocity = initMaxVelocity()
+    val accelerationTime = vehicle.acclerationTime
+    val passengers = initPassengers()
+    val xP = initXP()
+
+    private fun initPassengers(): Int {
         return vehicle.passengers * (1 + driver.defensiveDriving)
     }
 
-    private fun setAccelerationTime(): Int {
-        TODO("Not yet implemented")
+    private fun initMaxVelocity(): Int {
+        return when(vehicle.type){
+            "car" -> carMaxVelocity()
+            "motorcycle" -> motorcycleMaxVelocity()
+            else -> bikeMaxVelocity()
+        }
     }
 
-    private fun setMaxVelocity(): Int {
-        return 0
+    private fun bikeMaxVelocity(): Int {
+        return vehicle.maxAcceleration * driver.boldness
+    }
 
+    private fun motorcycleMaxVelocity(): Int {
+        return 1/vehicle.weight * vehicle.maxAcceleration
+
+    }
+
+    private fun carMaxVelocity(): Int {
+            return if (vehicle.style == "sedã") {
+               vehicle.maxAcceleration
+            } else{
+                vehicle.maxAcceleration + 10
+            }
+        }
+
+    private fun initXP(): Int{
+        return when(vehicle.type){
+            "car" -> driver.carXP
+            "motorcycle" ->driver.motorcycleXP
+            else ->driver.bikeXP
+        }
     }
 }
